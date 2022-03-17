30b5322badb8871965de9da86d7950ed29b34bf1
nguyenhuubaoan <66407644+nguyenhuubaoan@users.noreply.github.com>
2022-03-17 19:17:12 +0700

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,113 +1,142 @@
-#include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
-#define MAX 10 
-#define inputfile "C:/Users/Admin/Desktop/Test2/Test2.txt" 
-
-typedef struct GRAPH {
-	int n; 
-	int a[MAX][MAX]; 
+#include<stdio.h>
+#define MAX 100
+#define inputfile1 "M:/Test5_01.txt"
+#define inputfile2 "M:/Test5_02.txt"
+#define inputfile3 "M:/Test5_03.txt"
+typedef struct GRAPH
+{
+	int n;
+	int a[MAX][MAX];
 }DOTHI;
-
 int DocMaTranKe(char TenFile[100], DOTHI &g)
 {
-	FILE* f; 
-	f = fopen(TenFile, "rt"); 
-	if (f == NULL) 
+	FILE*f;
+	f=fopen(TenFile,"rt");
+	if(f==NULL)
 	{
 		printf("Khong mo duoc file\n");
-		return 0; 
-	}
-	fscanf(f, "%d", &g.n);
-	int i, j;
-	for (i=0; i<g.n; i++)
-	{
-		for (j=0; j<g.n; j++)
-	{
-	fscanf(f, "%d", &g.a[i][j]); 
-	}
-}
-fclose(f);
-return 1; 
-}
-
-void XuatMaTranKe (DOTHI g)
-{
-	printf("So dinh cua do thi la %d\n", g.n);
-	printf("Ma tran ke cua do thi la\n");
-	for (int i = 0; i < g.n; i++)
-	{
-		printf ("\t");
-		for (int j = 0; j < g.n; j++)
-	{
-	printf("%d ",g.a[i][j]);
-	}
-	printf("\n");
-	}
-}
-
-int KiemTraMaTranKeHopLe(DOTHI g)
-{
-	int i;
-	for (i=0; i<g.n; i++)
-	{
-		if (g.a[i][i] != 0) 
 		return 0;
 	}
-return 1; 
+	fscanf(f,"%d",&g.n);
+	int i,j;
+	for(i=0;i<g.n;i++)
+		for(j=0;j<g.n;j++)
+			fscanf(f,"%d",&g.a[i][j]);
+	fclose(f);
+	return 1;
 }
-
-void TimCacDinhLienThong (DOTHI g, int nhan[MAX], int i)
+void XuatMaTranKe(DOTHI g)
 {
-	for (int j = 0; j < g.n; j++)
+	printf("So dinh cua do thi la %d\n",g.n);
+	printf("Ma tran ke cua do thi la\n");
+	for(int i=0;i<g.n;i++)
 	{
-		if (g.a[i][j] != 0 && nhan[j] != nhan[i]) 
-		{
-			nhan[j] = nhan[i]; 
-			TimCacDinhLienThong (g, nhan, j); 
-		}
-	}
-}
-
-void XetLienThong(DOTHI g)
-{
-	int Nhan[MAX]; 
-	int i;
-	for (i=0;i<g.n;i++) 
-	Nhan[i] =0;
-	int SoThanhPhanLT = 0; 
-	for (i=1; i<g.n; i++)
-	{
-		if (Nhan[i] == 0) 
-		{
-			SoThanhPhanLT ++; 
-			Nhan[i] = SoThanhPhanLT; 
-			TimCacDinhLienThong(g, Nhan, i); 
-		}
-	}
-	printf ("So thanh phan lien thong la %d\n",SoThanhPhanLT);
-	for (i = 1; i <= SoThanhPhanLT; i++)
-	{
-		printf("Thanh phan lien thong thu %d gom cac dinh: %d", i,Nhan[i]);
+		for(int j=0;j<g.n;j++)
+			printf("%d\t",g.a[i][j]);
 		printf("\n");
 	}
 }
 
+int luuVet[MAX];
+int chuaXet[MAX];
+struct QUEUE
+{
+	int size,a[MAX];
+};
+void khoiTaoQueue(QUEUE &Q)
+{
+	Q.size=0;
+}
+int dayGiaTriVaoQueue(QUEUE &Q,int value)
+{
+	if(Q.size+1>=100)
+		return 0;
+	Q.a[Q.size]=value;
+	Q.size++;
+	return 1;
+}
+int layGiaTriRaKhoiQueue(QUEUE &Q,int &value)
+{
+	if(Q.size<=0)
+		return 0;
+	value=Q.a[0];
+	for(int i=0;i<Q.size-1;i++)
+		Q.a[i]=Q.a[i+1];
+	Q.size--;
+	return 1;
+}
+int kiemTraQueueRong(QUEUE Q)
+{
+	if(Q.size<=0)
+		return 1;
+	return 0;
+}
+void BFS(int v,DOTHI g)
+{
+	QUEUE Q;
+	khoiTaoQueue(Q);
+	dayGiaTriVaoQueue(Q,v);
+	while(!kiemTraQueueRong(Q))
+	{
+		layGiaTriRaKhoiQueue(Q,v);
+		chuaXet[v]=1;
+		for(int u=0;u<g.n;u++)
+			if(g.a[v][u]!=0&&chuaXet[u]==0)
+			{
+				dayGiaTriVaoQueue(Q,u);
+				if(luuVet[u]==-1)
+					luuVet[u]=v;
+			}
+	}
+}
+void duyetTheoBFS(int S,int F,DOTHI g)
+{
+	int i;
+	for(i=0;i<g.n;i++)
+	{
+		luuVet[i]=-1;
+		chuaXet[i]=0;
+	}
+	BFS(S,g);
+	if(chuaXet[F]==1)
+	{
+		printf("Duong di tu dinh %d den dinh %d theo BFS la: \n\t",S,F);
+		i=F;
+		printf("%d",F);
+		while(i!=S)
+		{
+			printf(" <--- %d",luuVet[i]);
+			i=luuVet[i];
+		}
+	}
+	else
+		printf("\nKhong co duong di tu dinh %d den dinh %d theo BFS\n",S,F);
+}
 int main()
 {
 	DOTHI g;
-	if (DocMaTranKe(inputfile, g) == 1)
+	int S,F;
+	if(DocMaTranKe(inputfile1, g) == 1)
 	{
-		printf("Da lay thong tin do thi tu file thanh cong.\n\n");
+		printf("Da lay thong tin do thi tu file thanh cong.\n");
 		XuatMaTranKe(g);
-		printf("Bam 1 phim bat ki de tien hanh kiem tra do thi ...\n\n");
-		getch();
-		if (KiemTraMaTranKeHopLe(g) == 1)
-		printf ("Do thi hop le.\n");
-		else
-		printf ("Do thi khong hop le.\n");
-		XetLienThong(g);
-		return 0;
+		printf("Nhap diem bat dau: ");
+		scanf("%d",&S);
+		printf("Nhap diem ket thuc: ");
+		scanf("%d",&F);
+		duyetTheoBFS(S,F,g);
+	}
+	if(DocMaTranKe(inputfile2, g) == 1)
+	{
+		printf("\nDa lay thong tin do thi tu file thanh cong.\n");
+		XuatMaTranKe(g);
+		duyetTheoBFS(0,5,g);
+	}
+	if(DocMaTranKe(inputfile3, g) == 1)
+	{
+		printf("\nDa lay thong tin do thi tu file thanh cong.\n");
+		XuatMaTranKe(g);
+		duyetTheoBFS(3,6,g);
 	}
 }
 
